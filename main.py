547dd928ffec495b9667cac5c5cd52b635baf6ed
alberto <albertopouquiros@gmail.com>
2015-12-13 12:13:46 +0100

    Pep 8

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 #! /usr/bin/python
 # coding=utf-8
 
+from SpanishCorpus import SpanishCorpus
+
 __author__ = "Alberto Pou Quirós"
 
-from SpanishCorpus import SpanishCorpus
 
 def main():
     text = u'Azkaban ha existido desde el siglo XV, pero en sus origenes no era una prisión.\
              La isla en el mar del Norte en la que se construyó la primera fortaléza no se documenta\
              en ningun mapa, muggle o mágico, y se cree que fue creada, o agrandada, por medios\
              mágicos.'
-    corpus = SpanishCorpus(text, timing=True)
+    corpus = SpanishCorpus.SpanishCorpus(text, timing=True)
     corpus.tokenize()
     corpus.clean()
-    corpus.filterStopwords()
+    corpus.filter_stop_words()
     corpus.analize(to_correct=True)
-    corpus.cleanPostAnalysis()
-    corpus.unifyTokens()
+    corpus.clean_post_analysis()
+    corpus.unify_tokens()
     corpus.synonymize()
-    corpus.calculateFrequencies()
-    corpus.showResults()
+    corpus.calculate_frequencies()
+    corpus.show_results()
+
 
 if __name__ == "__main__": main()
